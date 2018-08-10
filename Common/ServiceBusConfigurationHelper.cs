6fc5d2cb209360391715b9192c7f0a3586111c99
Ben Horrix <ben.horrix@jet2.com>
2018-08-10 15:07:00 +0100

    Added RabbitMQ transport

---
+++ Common/ServiceBusConfigurationHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Threading.Tasks;
+using NServiceBus;
+
+namespace Common
+{
+    public static class ServiceBusConfigurationHelper
+    {
+        public static async Task<IEndpointInstance> ConfigureEndpoint(string endpointName)
+        {
+            var endpointConfiguration = new EndpointConfiguration(endpointName);
+            endpointConfiguration.EnableInstallers();
+
+            var transport = endpointConfiguration.UseTransport<RabbitMQTransport>();
+            transport.UseConventionalRoutingTopology();
+            transport.ConnectionString("host=localhost:15672");
+
+            return await Endpoint.Start(endpointConfiguration).ConfigureAwait(false);
+        }
+    }
+}
